55a79fd7a3248e0526795e41204365f5dd0491be
Sarthak Gupta <sarthakgupta@umass.edu>
2026-08-18 22:47:58 -0400

    corrected line 22 to only show date

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ print(f"the return is: {(end_price - start_price)/start_price * 100:.2f}%")
 
 print(f"the volume mean is: {data['Volume'].mean()}")
 max_volume=data["Volume"].max()
-print(f"the date with the maximum volume is: {data[data['Volume']==max_volume]}")
+print(f"the date with the maximum volume is: {data[data['Volume']==max_volume]['Date']}")
